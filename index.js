ef47d659e7069b000a17e8c83575dd8de8e6a3e6
Mihail Dragnev <bymykel5@gmail.com>
2022-02-18 18:59:36 +0100

    Some refactoring and minor changes.

--- index.js
+++ index.js
@@ -2,8 +2,24 @@ import { skins, collectibles } from "./services/csgo.js";
 import { saveDataJson } from "./services/saveDataJson.js";
 
 (async () => {
-    const skinsData = await skins().then(response => JSON.stringify(response, null, 4));
-    const collectiblesData = await collectibles().then(response => JSON.stringify(response, null, 4));
+    const skinsData = await skins().then((response) => {
+        const skins = response.reduce(
+            (items, item) => ({
+                ...items,
+                [item.weapon_id]: {
+                    name: item.weapon,
+                    skins: [...(items[item.weapon_id]?.skins || []), item],
+                },
+            }),
+            {}
+        );
+
+        return JSON.stringify(Object.values(skins), null, 4);
+    });
+    
+    const collectiblesData = await collectibles().then((response) =>
+        JSON.stringify(response, null, 4)
+    );
 
     saveDataJson("public/api/skins.json", skinsData);
     saveDataJson("public/api/collectibles.json", collectiblesData);
