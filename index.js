e81a55857800629788acd18945cf12f42791a2e3
Mihail Dragnev <bymykel5@gmail.com>
2022-02-19 17:49:05 +0100

    Some refactoring.

--- index.js
+++ index.js
@@ -9,34 +9,12 @@ import {
 import { saveDataJson } from "./services/saveDataJson.js";
 
 (async () => {
-    const skinsData = await skins().then((response) =>
-        JSON.stringify(response, null, 4)
-    );
+    const functions = [skins, stickers, collectibles, collections, cases, keys];
 
-    const stickersData = await stickers().then((response) =>
-        JSON.stringify(response, null, 4)
-    );
+    for (const i in functions) {
+        const data = await functions[i]();
+        const json = JSON.stringify(data, null, 4);
 
-    const collectiblesData = await collectibles().then((response) =>
-        JSON.stringify(response, null, 4)
-    );
-
-    const collectionsData = await collections().then((response) =>
-        JSON.stringify(response, null, 4)
-    );
-
-    const casesData = await cases().then((response) =>
-        JSON.stringify(response, null, 4)
-    );
-
-    const keysData = await keys().then((response) =>
-        JSON.stringify(response, null, 4)
-    );
-
-    saveDataJson("public/api/skins.json", skinsData);
-    saveDataJson("public/api/stickers.json", stickersData);
-    saveDataJson("public/api/collectibles.json", collectiblesData);
-    saveDataJson("public/api/collections.json", collectionsData);
-    saveDataJson("public/api/cases.json", casesData);
-    saveDataJson("public/api/keys.json", keysData);
+        saveDataJson(`public/api/${functions[i].name}.json`, json);
+    }
 })();
