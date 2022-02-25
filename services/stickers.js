8cfb1af3c82f1375907524b03e86253b0f5229c3
Mihail Dragnev <bymykel5@gmail.com>
2022-02-25 23:12:34 +0100

    Move stickers to individual service.

---
+++ services/stickers.js
@@ -0,0 +1,39 @@
+import { IMAGES_BASE_URL } from "../utils/config.js";
+import { saveDataJson } from "./saveDataJson.js";
+import { getTranslation } from "./translations.js";
+
+const isSticker = (item) => {
+    if (item.sticker_material === undefined) {
+        return false;
+    }
+    if (!item.item_name.startsWith("#StickerKit_")) {
+        return false;
+    }
+    if (item.name.includes("graffiti")) {
+        return false;
+    }
+
+    return true;
+};
+
+const parseItem = (item, translations) => {
+    const image = `${IMAGES_BASE_URL}econ/stickers/${item.sticker_material.toLowerCase()}_large.png`;
+
+    return {
+        id: item.item_name.replace("#StickerKit_", ""),
+        name: getTranslation(translations, item.item_name),
+        description: getTranslation(translations, item.description_string),
+        rarity: getTranslation(translations, `rarity_${item.item_rarity}`),
+        image,
+    };
+};
+
+export const getStickers = (stickerKits, translations) => {
+    const stickers = [];
+
+    stickerKits.forEach((item) => {
+        if (isSticker(item)) stickers.push(parseItem(item, translations));
+    });
+
+    saveDataJson(`./public/api/stickers.json`, stickers);
+};
