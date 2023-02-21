883d8accb5447edb5a79df3d5edb4f313d7b4fd0
Mihail Dragnev <bymykel5@gmail.com>
2023-02-21 19:51:41 +0100

    feat: new folders name.

--- utils/config.js
+++
@@ -1,8 +0,0 @@
-export const ITEMS_GAME_URL =
-    "https://raw.githubusercontent.com/SteamDatabase/GameTracking-CSGO/master/csgo/scripts/items/items_game.txt";
-
-export const CSGO_ENGLISH_URL =
-    "https://raw.githubusercontent.com/SteamDatabase/GameTracking-CSGO/master/csgo/resource/csgo_english.txt";
-
-export const IMAGES_BASE_URL =
-    "https://raw.githubusercontent.com/SteamDatabase/GameTracking-CSGO/master/csgo/pak01_dir/resource/flash/";
