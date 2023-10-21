4af7fa96a2386336b060bae8643afbcaf579267b
ByMykel <bymykel5@gmail.com>
2023-10-21 12:41:26 +0200

    feat: add stattrak music kits

--- services/musicKits.js
+++ services/musicKits.js
@@ -8,7 +8,7 @@ const parseItem = (item) => {
     const image = cdn[`${item.image_inventory.toLowerCase()}`];
     const exclusive = isExclusive(item.name);
 
-    return {
+    const normalMusicKit = {
         id: `music_kit-${item.object_id}`,
         name: exclusive ? $t(item.loc_name) : $t(item.coupon_name),
         description: $t(item.loc_description),
@@ -19,13 +19,33 @@ const parseItem = (item) => {
         exclusive,
         image,
     };
+
+    if ($t(`${item.coupon_name}_stattrak`)) {
+        const stattrakMusicKit = {
+            id: `music_kit-${item.object_id}_st`,
+            name: $t(`${item.coupon_name}_stattrak`),
+            description: $t(item.loc_description),
+            rarity: {
+                id: "rarity_rare",
+                name: $t("rarity_rare"),
+            },
+            exclusive: false,
+            image,
+        };
+
+        return [normalMusicKit, stattrakMusicKit];
+    }
+
+    return [normalMusicKit];
 };
 
 export const getMusicKits = () => {
     const { musicDefinitions } = state;
     const { folder } = languageData;
 
-    const musicKits = musicDefinitions.map(parseItem);
+    const musicKits = musicDefinitions
+        .map(parseItem)
+        .reduce((acc, kits) => acc.concat(kits), []);
 
     saveDataJson(`./public/api/${folder}/music_kits.json`, musicKits);
 };
