c1434d6a89d3ecf1c6730f70fe4fd646bbe41e54
ByMykel <bymykel5@gmail.com>
2025-03-30 14:39:13 +0200

    wip: add more items.

--- services/inventory.js
+++ services/inventory.js
@@ -3,13 +3,13 @@ import path from "path";
 import { saveDataJson } from "../utils/saveDataJson.js";
 import { languageData } from "./translations.js";
 
-const formatInventoryData = ({ skins, crates }) => {
+const formatInventoryData = ({ skins, crates, collectibles, stickers, graffiti, musicKits }) => {
     const items = {}
 
     skins.forEach((skin) => {
         if (!items['skins']) items['skins'] = {};
         if (!items['skins'][skin.weapon.weapon_id]) items['skins'][skin.weapon.weapon_id] = {};
-        
+
         items['skins'][skin.weapon.weapon_id][skin.paint_index] = {
             name: skin.name,
             market_hash_name: skin.market_hash_name,
@@ -26,6 +26,52 @@ const formatInventoryData = ({ skins, crates }) => {
             market_hash_name: crate.market_hash_name,
             image: crate.image,
         };
+    });
+
+    collectibles.forEach((collectible) => {
+        if (!items['collectibles']) items['collectibles'] = {};
+        if (!items['collectibles'][collectible.id.replace('collectible-', '')]) items['collectibles'][collectible.id.replace('collectible-', '')] = {};
+
+        items['collectibles'][collectible.id.replace('collectible-', '')] = {
+            name: collectible.name,
+            market_hash_name: collectible.market_hash_name,
+            image: collectible.image,
+        };
+    });
+
+    stickers.forEach((sticker) => {
+        if (!items['stickers']) items['stickers'] = {};
+        if (!items['stickers'][sticker.id.replace('sticker-', '')]) items['stickers'][sticker.id.replace('sticker-', '')] = {};
+
+        items['stickers'][sticker.id.replace('sticker-', '')] = {
+            name: sticker.name,
+            market_hash_name: sticker.market_hash_name,
+            image: sticker.image,
+        };
+    });
+
+    graffiti.forEach((graffiti) => {
+        if (!items['graffiti']) items['graffiti'] = {};
+        if (!items['graffiti'][graffiti.id.replace('graffiti-', '')]) items['graffiti'][graffiti.id.replace('graffiti-', '')] = {};
+
+        items['graffiti'][graffiti.id.replace('graffiti-', '')] = {
+            name: graffiti.name,
+            market_hash_name: graffiti.market_hash_name,
+            image: graffiti.image,
+        };
+    });
+
+    musicKits.forEach((musicKit) => {
+        if (!musicKit.id.includes('_st')) {
+            if (!items['music_kits']) items['music_kits'] = {};
+            if (!items['music_kits'][musicKit.id.replace('music_kit-', '')]) items['music_kits'][musicKit.id.replace('music_kit-', '')] = {};
+
+            items['music_kits'][musicKit.id.replace('music_kit-', '')] = {
+                name: musicKit.name,
+                market_hash_name: musicKit.market_hash_name,
+                image: musicKit.image,
+            };
+        }
     });
 
     return items;
@@ -45,11 +91,19 @@ export const getInventory = async () => {
     const { folder } = languageData;
     const skinsFilePath = path.join(process.cwd(), `./public/api/${folder}/skins.json`);
     const cratesFilePath = path.join(process.cwd(), `./public/api/${folder}/crates.json`);
+    const collectiblesFilePath = path.join(process.cwd(), `./public/api/${folder}/collectibles.json`);
+    const stickersFilePath = path.join(process.cwd(), `./public/api/${folder}/stickers.json`);
+    const graffitiFilePath = path.join(process.cwd(), `./public/api/${folder}/graffiti.json`);
+    const musicKitsFilePath = path.join(process.cwd(), `./public/api/${folder}/music_kits.json`);
 
     try {
         const skins = await waitForFile(skinsFilePath);
         const crates = await waitForFile(cratesFilePath);
-        const inventory = formatInventoryData({ skins, crates });
+        const collectibles = await waitForFile(collectiblesFilePath);
+        const stickers = await waitForFile(stickersFilePath);
+        const graffiti = await waitForFile(graffitiFilePath);
+        const musicKits = await waitForFile(musicKitsFilePath);
+        const inventory = formatInventoryData({ skins, crates, collectibles, stickers, graffiti, musicKits });
         saveDataJson(`./public/api/${folder}/inventory.json`, inventory);
     } catch (error) {
         console.error(error.message);
