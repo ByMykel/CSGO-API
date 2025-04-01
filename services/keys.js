a43d3d52ad82078d6d92463770f7b7f49d3a773c
ByMykel <bymykel5@gmail.com>
2025-04-01 21:14:16 +0200

    feat: improve keys.

--- services/keys.js
+++ services/keys.js
@@ -34,6 +34,48 @@ const isKey = (item) => {
 const parseItem = (item) => {
     const { items } = state;
 
+    const marketable = [
+        "#CSGO_Tool_WeaponCase_Key",
+        "#CSGO_esports_crate_key_1",
+        "#CSGO_sticker_crate_key_1",
+        "#CSGO_community_crate_key_1",
+        "#CSGO_community_crate_key_2",
+        "#CSGO_sticker_crate_key_community01",
+        "#CSGO_community_crate_key_3",
+        "#CSGO_community_crate_key_4",
+        "#CSGO_community_crate_key_5",
+        "#CSGO_community_crate_key_6",
+        "#CSGO_community_crate_key_7",
+        "#CSGO_community_crate_key_8",
+        "#CSGO_community_crate_key_9",
+        "#CSGO_crate_community_10_key",
+        "#CSGO_crate_key_community_11",
+        "#CSGO_crate_key_community_12",
+        "#CSGO_crate_key_community_13",
+        "#CSGO_crate_key_gamma_2",
+        "#CSGO_crate_key_community_15",
+        "#CSGO_crate_key_community_16",
+        "#CSGO_crate_key_community_17",
+        "#CSGO_crate_key_community_18",
+        "#CSGO_crate_key_community_19",
+        "#CSGO_crate_key_community_20",
+        "#CSGO_crate_key_community_21",
+        "#CSGO_crate_key_community_22",
+        // "#CSGO_crate_key_community_23",
+        "#CSGO_crate_key_community_24",
+        // "#CSGO_crate_key_community_25",
+        // "#CSGO_crate_key_community_26",
+        // "#CSGO_crate_key_community_27",
+        // "#CSGO_crate_key_community_28",
+        // "#CSGO_crate_key_community_29",
+        // "#CSGO_crate_key_community_30",
+        // "#CSGO_crate_key_community_31",
+        // "#CSGO_crate_key_community_32",
+        // "#CSGO_crate_key_community_33",
+        // "#CSGO_crate_key_community_34",
+        // "#CSGO_crate_key_community_35",
+    ]
+
     const image = getImageUrl(item.image_inventory.toLowerCase());
     const crates = Object.values(items)
         .filter(
@@ -52,8 +94,9 @@ const parseItem = (item) => {
         name: $t(item.item_name),
         description:
             $t(item.item_description) ?? $t(item.item_description_prefab),
-        market_hash_name: $t(item.item_name, true),
         crates,
+        market_hash_name: marketable.includes(item.item_name) ? $t(item.item_name, true) : null,
+        marketable: marketable.includes(item.item_name),
         image,
     };
 };
