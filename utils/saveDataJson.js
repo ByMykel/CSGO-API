c42ae97a8183be1a807e44c44dde2e8308f0bc96
ByMykel <bymykel5@gmail.com>
2025-09-23 16:39:24 +0200

    ci: test a new workflow to update and groupa at the same time

--- utils/saveDataJson.js
+++ utils/saveDataJson.js
@@ -1,29 +1,34 @@
 import * as fs from "fs";
 
 export const saveDataJson = (file, data) => {
-    // I beautify the JSON data because it's easier for me see the changes
-    const json = JSON.stringify(data, null, 1);
+    return new Promise((resolve, reject) => {
+        // I beautify the JSON data because it's easier for me see the changes
+        const json = JSON.stringify(data, null, 1);
 
-    const folders = file
-        .replace(/\.\/public\/api\/(.*)\/(.*)\.json/, "$1")
-        .split("/");
+        const folders = file
+            .replace(/\.\/public\/api\/(.*)\/(.*)\.json/, "$1")
+            .split("/");
 
-    // Create api folder if it doesn't exist
-    if (!fs.existsSync("./public/api")) {
-        fs.mkdirSync("./public/api");
-    }
-
-    folders.forEach((folder, index) => {
-        const path = folders.slice(0, index + 1).join("/");
-
-        if (!fs.existsSync(`./public/api/${path}`)) {
-            fs.mkdirSync(`./public/api/${path}`);
+        // Create api folder if it doesn't exist
+        if (!fs.existsSync("./public/api")) {
+            fs.mkdirSync("./public/api");
         }
-    });
 
-    fs.writeFile(file, json, (err) => {
-        if (err) throw err;
+        folders.forEach((folder, index) => {
+            const path = folders.slice(0, index + 1).join("/");
 
-        // console.log(`JSON data is saved in ${file}.`);
+            if (!fs.existsSync(`./public/api/${path}`)) {
+                fs.mkdirSync(`./public/api/${path}`);
+            }
+        });
+
+        fs.writeFile(file, json, (err) => {
+            if (err) {
+                reject(err);
+            } else {
+                // console.log(`JSON data is saved in ${file}.`);
+                resolve();
+            }
+        });
     });
 };
